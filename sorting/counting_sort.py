53d87e7f4b98df9d54d2c0d690db954a8acacdf8
dangoldbj <dangoldbj23@gmail.com>
2023-03-10 13:03:12 +0100

    add counting sort

---
+++ sorting/counting_sort.py
@@ -0,0 +1,34 @@
+
+# for i in range(len(pos)):
+#     t = pos[i]
+#     pos[i] = s
+#     s += t
+
+# s = len(pos) - 1
+# for i in range(len(pos) -1 , -1, -1):
+#     pos[i] = s - pos[i]
+#     s = pos[i]
+# print(pos)
+
+#TODO: negative values
+#TODO: Hashing
+
+def counting_sort(a):
+    k = max(a) + 1
+    pos = [0] * k
+    for el in a:
+        pos[el] += 1
+
+    s = 0
+    for i in range(k):
+        pos[i], s = s, s + pos[i]
+    
+    out = [0] * len(a)
+    for i in range(len(a)):
+        out[pos[a[i]]] = a[i]
+        pos[a[i]] += 1
+    return out
+
+if __name__ == '__main__':
+    l = [4,1,2,3,4,5, 3]
+    print(counting_sort(l))
